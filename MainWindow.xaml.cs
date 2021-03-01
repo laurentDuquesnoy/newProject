24e881fc4acc88a051999f544bc49f90b876b58a
Developer0108 <laurentduquesnoy5@gmail.com>
2021-03-01 10:17:43 +0100

    counter added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace newProject
         {
             InitializeComponent();
         }
+        public int Counter = 0;
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            lblCounter.Content = Counter;
+        }   
+        
+        
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Counter++;
+            lblCounter.Content = Counter;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Counter--;
+            lblCounter.Content = Counter;
+        }
     }
 }
